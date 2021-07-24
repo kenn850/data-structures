49c34d3f4d3c627e94ae65179dc8c3b54b942b3a
Kennith Philips <35496496+kenn850@users.noreply.github.com>
2021-07-24 19:40:41 +0530

    Update Merge Intervals.cpp

--- array/Merge Intervals.cpp
+++ array/Merge Intervals.cpp
@@ -1,3 +1,5 @@
+// ----------------------------------------------- Merge Intervals --------------------------------------------
+
 // Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
 
 // You may assume that the intervals were initially sorted according to their start times.
