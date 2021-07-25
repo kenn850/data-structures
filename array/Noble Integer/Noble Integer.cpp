b303662ce10c16dff6cd5a715e96283a8b27a1a2
Kennith Philips <35496496+kenn850@users.noreply.github.com>
2021-07-25 22:07:31 +0530

    Noble Integer

---
+++ array/Noble Integer/Noble Integer.cpp
@@ -0,0 +1,15 @@
+
+
+int Solution::solve(vector<int> &A) {
+    sort(A.begin(),A.end());
+    bool flag = 0;
+    for(int i = 0; i < A.size(); i++){
+        if(i!=A.size() -1 && A[i+1] == A[i]) continue;
+        if(A[i] == A.size()-i-1){
+            flag = 1;
+            break;
+        }
+    }
+    if(flag) return 1;
+    else return -1;
+}
