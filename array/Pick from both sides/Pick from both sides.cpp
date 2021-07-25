9cc642f3cc415382d5df7b202f8939b1b87ec164
Kennith Philips <35496496+kenn850@users.noreply.github.com>
2021-07-25 22:10:54 +0530

    Pick from both sides

---
+++ array/Pick from both sides/Pick from both sides.cpp
@@ -0,0 +1,17 @@
+
+int Solution::solve(vector<int> &A, int B) {
+
+    int s = A.size();
+    int max_Here = 0;
+    for(int i = 0; i<B;i++){
+        max_Here += A[i]; 
+    }
+    int ans = max_Here;
+    int j = B-1;
+    for(int i = s-1; i>s-B-1;i--){
+        if(max_Here - A[j] + A[i] > ans) ans = max_Here - A[j] + A[i];
+        max_Here = max_Here - A[j] + A[i];
+        j--;
+    }
+    return ans;
+}
