2f4324b87f02f5ca3a5941192998385a3b7566e2
Kennith Philips <35496496+kenn850@users.noreply.github.com>
2021-07-25 22:09:19 +0530

    Perfect Peak of Array

---
+++ array/Perfect Peak of Array/Perfect Peak of Array.cpp
@@ -0,0 +1,22 @@
+
+
+
+int Solution::perfectPeak(vector<int> &A) {
+    int leftBig =  A[0];
+    bool flag = 0;
+    for(int i = 1; i< A.size()-1; i++){
+        if(A[i] > leftBig){
+            leftBig = A[i];
+            if(A[i] < *min_element(A.begin()+i+1,A.end())){
+                flag = 1;
+                break;
+            }
+        }
+    }
+    if(flag){
+        return 1;
+    }
+    else
+        return 0;
+
+}
