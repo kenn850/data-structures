39de62800b62a5ec1d4a696c113e39349cf56456
Kennith Philips <35496496+kenn850@users.noreply.github.com>
2021-07-25 21:56:24 +0530

    Balance Array

---
+++ array/Balance Array/Balance Array.cpp
@@ -0,0 +1,21 @@
+int Solution::solve(vector<int> &A) {
+    int odd = 0, even = 0;
+    for(int i = 0; i<A.size(); i++){
+        if(i%2==0) even += A[i];
+        else odd += A[i];
+    }
+    int count = 0;
+    for(int i =0;i<A.size();i+=1){
+        if(i%2 == 0){
+            even -= A[i];
+            if(even==odd) count++;
+            odd+= A[i];
+        }
+        else{
+            odd -= A[i];
+            if(odd==even) count++;
+            even+= A[i];
+        }
+    }
+    return count;
+}
