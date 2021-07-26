7837016db1563d2f78d6c8bbeb85a21803ac8413
Kennith Philips <35496496+kenn850@users.noreply.github.com>
2021-07-26 19:24:31 +0530

    Min Steps in Infinite Grid

---
+++ array/Min Steps in Infinite Grid/Min Steps in Infinite Grid.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+// Class Definition 
+
+class Solution{
+    public:
+    int coverPoints( vector<int> &A ,vector<int> &B);
+};
+
+// Program Code
+
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+    int count = 0;
+    int x = 0; int y = 0;
+    for (int i=0; i < (A.size()-1); ++i) {
+        x = abs((A[i+1]-A[i]));
+        y = abs((B[i+1]-B[i]));
+        count += max(x,y);
+    }
+    return count;
+}
